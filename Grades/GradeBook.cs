8a0b1e05269e4b789e02caf2068bbb3ec5a5c8a1
Philip Bowles <pbowles@missouristate.edu>
2016-12-20 09:49:34 -0600

    Add updated GradeBook functionality.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            if (grades.Count > 0)
+            {
+                stats.AverageGrade = sum / grades.Count;
+            }
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<float> grades;
+
+        public static float MinimumGrade = 0;
+        public static float MaximumGrade = 100;
+    }
+}
